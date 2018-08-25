bf37211bbf6d7bf2a929fb4c068497cbdfc93279
Enrique <enrique@gmail.com>
2018-08-25 14:24:56 -0500

    se agrego la programacion en php

--- index.php
+++ index.php
@@ -1,1 +1,57 @@
-# Practica tarea
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Tarea para subir al repositorio de git Hub
+	</title>
+</head>
+<body>
+<header>TAREA!!</header>
+<section>
+	<article>
+		<p>
+			A continuación te voy a mostrar el abecedario realizado con estructura while
+		
+		<?php
+		echo "<br>";
+		$letra='a';
+
+		while ($letra<'z') {
+			echo ($letra." ");
+			$letra++;
+		}
+		?>
+		</p>
+	</aside>
+</p>
+Ahora se van a mostrar las tablas de multiplicar con do while
+<?php
+echo "<br>";
+echo "<br>";
+$j=1;
+do
+{
+	echo ("1X".$j."=".(1*$j)."<br>");
+	echo ("2X".$j."=".(2*$j)."<br>");
+	echo ("3X".$j."=".(3*$j)."<br>");
+	echo ("4X".$j."=".(4*$j)."<br>");
+	echo ("5X".$j."=".(5*$j)."<br>");
+	echo ("6X".$j."=".(6*$j)."<br>");
+	echo ("7X".$j."=".(7*$j)."<br>");
+	echo ("8X".$j."=".(8*$j)."<br>");
+	echo ("9X".$j."=".(9*$j)."<br>");
+	echo ("10X".$j."=".(10*$j)."<br>");
+	echo "<br>";
+	echo "<br>";
+	$j++;
+
+
+}while ($j<=10) 
+?>
+</p>
+</aside>
+	</article>
+</section>
+<div id='footer'>by Enrique Ruiz</div>
+</body>
+</html>
